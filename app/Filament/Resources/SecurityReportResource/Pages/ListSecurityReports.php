f6a803393453ad94e03e7f9e84d87bcb5e15627b
Juan Daniel <mail.jdnielss@gmail.com>
2024-12-06 18:32:36 +0700

    add: label create

--- app/Filament/Resources/SecurityReportResource/Pages/ListSecurityReports.php
+++ app/Filament/Resources/SecurityReportResource/Pages/ListSecurityReports.php
@@ -13,7 +13,7 @@ class ListSecurityReports extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label("Create"),
         ];
     }
 }
