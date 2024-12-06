f703b7d1fcba34cb7479136147b50502eb8697be
Juan Daniel <mail.jdnielss@gmail.com>
2024-12-06 17:18:16 +0700

    add: coming soon page

--- resources/views/join.blade.php
+++ resources/views/join.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SGB - Sharing Gils Blog</title>
+    <title>SGB - Form Join</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <script src="https://cdn.tailwindcss.com"></script>
+
 </head>
 <body>
 <div class="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'member'], function () {
+   Route::get('/join', function () {
+       return view('join');
+   });
+    Route::get('/rejoin', function () {
+        return view('rejoin');
+    });
+});
