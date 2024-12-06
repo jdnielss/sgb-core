c88d61082d35c3f4ffab31ede36049e10a56d47b
Juan Daniel <mail.jdnielss@gmail.com>
2024-12-06 18:32:55 +0700

    chore: remove unused import

--- app/Filament/Resources/SecurityReportResource/Pages/CreateSecurityReport.php
+++ app/Filament/Resources/SecurityReportResource/Pages/CreateSecurityReport.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\SecurityReportResource\Pages;
 
 use App\Filament\Resources\SecurityReportResource;
 use App\Models\Logging;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSecurityReport extends CreateRecord
